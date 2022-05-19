98ca0db6f3aeeb44c8c242d251cf48dd8c55a00e
Mohancap <muralimohanmp03@gmail.com>
2022-05-19 15:42:32 +0530

    added tables code

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+ 
+setuptools.setup(
+name='python_package_table',
+version='0.1',
+description='To get tables by using numbers ',
+url='',  
+author='Murali Mohan',
+author_email='',
+license='MIT', 
+keywords='table', 
+packages=setuptools.find_packages(),
+install_requires=[''],
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Education',
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3',
+],
+)
